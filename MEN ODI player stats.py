96203ef0afb2cdda70559a223768001365b5a9ca
mythen211 <151863226+mythen211@users.noreply.github.com>
2023-11-24 13:10:40 +0000

    Add files via upload

--- MEN ODI player stats.py
+++ MEN ODI player stats.py
@@ -10,10 +10,15 @@ df1 = df[filt1]
 
 filt2 = ((df1['Innings Date'] < date2) & (df1['Innings Date'] > date1))
 df2 = df1[filt2]
-df2.to_csv('date filter.csv')
+df2['Innings Runs Scored Num'] = df2['Innings Runs Scored Num'].astype(int)
 
-pivot = pd.pivot_table(df2,values='Innings Runs Scored Num',
-                       index='Innings Player',
-                       aggfunc='mean')
-print(pivot)
+df3 = df2.sort_values(by='Innings Runs Scored Num', ascending=False)
+df4 = df3['Innings Player','Innings Runs Scored Num','Innings Date']
+df4.to_csv('date filter.csv')
+
+
+#pivot = pd.pivot_table(df2,values='Innings Runs Scored Num',
+ #                     index='Innings Player',
+  #                     aggfunc='mean')
+#print(pivot)
 
